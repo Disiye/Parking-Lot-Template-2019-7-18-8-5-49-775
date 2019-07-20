9b7f040b8b54828142458837774767e9ec231cfb
WENGEM <emilio.wang@oocl.com>
2019-07-20 11:10:51 +0800

    [emilio] Update flyway sql, create parking lot table

---
+++ src/main/resources/db/migration/V2__CREATE_PARKING_ORDER_TABLE.sql
@@ -0,0 +1,8 @@
+create table PARKING_ORDER (
+    ID varchar(100) primary key,
+    PARKING_LOT_NAME varchar(100),
+    CAR_LICENSE_NUM varchar(100),
+    BEGIN_DATE date,
+    END_DATE date,
+    STATUS BOOLEAN
+);
